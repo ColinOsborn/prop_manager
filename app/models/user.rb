0f4a9789eb067709e045b1d13fe965f63f9ba5cb
Colin Osborn <colin.osborn@gmail.com>
2023-11-02 23:11:40 -0600

    added assocation to hoa in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  # Validations
   validates :name, presence: true
+  validates_length_of :name, minimum: 3, maximum: 25, message: 'Name must be between 3 to 25 characters'
   validates :email, presence: true
   validates :street_address, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
   validates :phone_number, presence: true
+
+  # Associations
+  belongs_to :housing_association
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
