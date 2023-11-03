6773f2729fd202e48bf2a941b41d6339b0cdb9d3
Colin Osborn <colin.osborn@gmail.com>
2023-11-02 21:00:26 -0600

    cleaning up some of the dashboard code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,47 +5,81 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create!(
-  'email'=>'user1@example.com',
-  'name'=>'Mark Otto',
-  'phone_number'=>'9705556826',
-  'street_address'=>'123 Field Street',
-  'state'=>'Colorado',
-  'zip_code'=>'81623',
-  'sign_in_count'=>3,
-  'city'=>'Basalt'
+User.create!(
+  email: 'user1@example.com',
+  name: 'Mark Otto',
+  phone_number: '9705556826',
+  street_address: '123 Field Street',
+  state: 'Colorado',
+  zip_code: '81623',
+  sign_in_count: 3,
+  city: 'Basalt',
+  password: 'password',
+  password_confirmation: 'password'
 )
 puts 'User1 has been created!'
-user2 = User.create!(
-  'email'=>'user2@example.com',
-  'name'=>'Jacob Thornton',
-  'phone_number'=>'9705556826',
-  'street_address'=>'225 Pine Street',
-  'state'=>'Colorado',
-  'zip_code'=>'81621',
-  'sign_in_count'=>3,
-  'city'=>'Aspen'
+User.create!(
+  email: 'user2@example.com',
+  name: 'Jacob Thornton',
+  phone_number: '9705556826',
+  street_address: '225 Pine Street',
+  state: 'Colorado',
+  zip_code: '81621',
+  sign_in_count: 3,
+  city: 'Aspen',
+  password: 'password',
+  password_confirmation: 'password'
 )
 puts 'User2 has been created!'
-user3 = User.create!(
-  'email'=>'user3@example.com',
-  'name'=>'Martin Smith',
-  'phone_number'=>'9705556826',
-  'street_address'=>'1310 Elm Street',
-  'state'=>'Colorado',
-  'zip_code'=>'81621',
-  'sign_in_count'=>5,
-  'city'=>'Aspen'
+User.create!(
+  email: 'user3@example.com',
+  name: 'Martin Smith',
+  phone_number: '9705556826',
+  street_address: '1310 Elm Street',
+  state: 'Colorado',
+  zip_code: '81621',
+  sign_in_count: 5,
+  city: 'Aspen',
+  password: 'password',
+  password_confirmation: 'password'
 )
 puts 'User3 has been created!'
-user4 = User.create!(
-  'email'=>'user4@example.com',
-  'name'=>'Kate Mayers',
-  'phone_number'=>'9705556826',
-  'street_address'=>'1310 Elm Street',
-  'state'=>'Colorado',
-  'zip_code'=>'81621',
-  'sign_in_count'=>5,
-  'city'=>'Aspen'
+User.create!(
+  email: 'user4@example.com',
+  name: 'Kate Mayers',
+  phone_number: '9705556826',
+  street_address: '1310 Elm Street',
+  state: 'Colorado',
+  zip_code: '81621',
+  sign_in_count: 5,
+  city: 'Aspen',
+  password: 'password',
+  password_confirmation: 'password'
 )
 puts 'User4 has been created!'
+User.create!(
+  email: 'user5@example.com',
+  name: 'Delilah King',
+  phone_number: '9705556826',
+  street_address: '10 Park Ave',
+  state: 'Colorado',
+  zip_code: '81621',
+  sign_in_count: 8,
+  city: 'Aspen',
+  password: 'password',
+  password_confirmation: 'password'
+)
+puts 'User5 has been created!'
+User.create!(
+  email: 'user6@example.com',
+  name: 'Cat Crazy',
+  phone_number: '9705556826',
+  street_address: '110 Pines Ave',
+  state: 'Colorado',
+  zip_code: '81621',
+  sign_in_count: 8,
+  city: 'Basalt',
+  password: 'password',
+  password_confirmation: 'password'
+)
+puts 'User6 has been created!'
--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DashboardsController, type: :controller do
-  describe 'GET Show' do
+  describe 'GET show' do
     it 'renders the show template' do
       get :show
       expect(response).to render_template('show')
