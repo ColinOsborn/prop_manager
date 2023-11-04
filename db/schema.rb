53ffa13aeee5e5d3da63dcadaeb605e35ad8c048
Colin Osborn <colin.osborn@gmail.com>
2023-11-03 20:11:05 -0600

    implemented migration for laborer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_04_015530) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_020937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_04_015530) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "laborers", force: :cascade do |t|
+    t.string "photo"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.text "bio"
+    t.string "experience"
+    t.integer "user_id"
+    t.integer "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_laborers_on_account_id"
+    t.index ["user_id"], name: "index_laborers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
